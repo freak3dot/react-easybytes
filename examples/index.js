81b1d682d985ca157242b6d523ef0b00e06eb4f1
Ryan Johnston <ryan.johnston@thepinnaclesolutions.com>
2017-03-13 17:12:55 -0400

    Add a couple more examples.

--- examples/index.js
+++ examples/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 
 import EasyBytes from '../src/js/EasyBytes';
 
-ReactDOM.render(<EasyBytes />, document.getElementById('mount'));
+ReactDOM.render(<EasyBytes id="easybytes" />, document.getElementById('mount1'));
+
+ReactDOM.render(<EasyBytes id="easybytes-with-default" defaultValue="3072" />, document.getElementById('mount2'));
+
+ReactDOM.render(<EasyBytes id="easybytes-with-default" defaultValue="3072" inputClass="form-control" selectClass="form-control" />, document.getElementById('mount3'));
