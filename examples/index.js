b8c8092cb226984dbdd92d40e2f87b794b78e3b4
Ryan Johnston <github@shopandlearn.net>
2017-09-29 16:36:35 -0400

    Update index.js
    Add examle with multiple.

--- examples/index.js
+++ examples/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 
 import EasyBytes from '../src/js/EasyBytes';
 
-ReactDOM.render(<EasyBytes id="easybytes" />, document.getElementById('mount1'));
+ReactDOM.render(<EasyBytes id="easybytes" name="default" />, document.getElementById('mount1'));
 
-ReactDOM.render(<EasyBytes id="easybytes-with-default" defaultValue="3072" />, document.getElementById('mount2'));
+ReactDOM.render(<EasyBytes id="easybytes-with-default" defaultValue={3072} name="preset" />, document.getElementById('mount2'));
 
-ReactDOM.render(<EasyBytes id="easybytes-with-default" defaultValue="3072" inputClass="form-control" selectClass="form-control" />, document.getElementById('mount3'));
+ReactDOM.render(<EasyBytes id="easybytes-with-default" defaultValue={3072} name="preset" multiple={1024} />, document.getElementById('mount3'));
+
+ReactDOM.render(<EasyBytes id="easybytes-with-default" defaultValue={3072} inputClass="form-control" selectClass="form-control" name="bootstrap" />, document.getElementById('mount4'));
